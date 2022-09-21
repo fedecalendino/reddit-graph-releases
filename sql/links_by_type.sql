fcddcb9d3232f40e6daedaf0b5fe0787ff90de99
Fede Calendino <fede@calendino.com>
2022-09-21 10:16:21 +0200

    Minor change on sql

--- sql/links_by_type.sql
+++ sql/links_by_type.sql
@@ -1,11 +1,11 @@
 (
-	SELECT 'TOTAL', COUNT(*) AS amount
+	SELECT 'TOTAL' AS type_, COUNT(*) AS amount
 		FROM links LEFT JOIN subreddits ON links.target = subreddits.name
 			WHERE subreddits.type NOT IN ('non-existent', 'deleted')
 )
 UNION 
 (
-	SELECT links.type, COUNT(*) AS amount
+	SELECT links.type AS type_, COUNT(*) AS amount
 		FROM links LEFT JOIN subreddits ON links.target = subreddits.name
 			WHERE subreddits.type NOT IN ('non-existent', 'deleted')
 				GROUP BY links.type
