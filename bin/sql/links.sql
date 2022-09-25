7e824df421c4b20a326d1b9d6b7a94e59b3f355a
Fede Calendino <fede@calendino.com>
2022-09-25 12:43:13 +0200

    Remove bin files

--- bin/sql/links.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-		links.source,
-		links.type,
-		links.target,
-		links.updated_at
-	FROM links LEFT JOIN subreddits ON links.target = subreddits.name
-		WHERE subreddits.type NOT IN ('non-existent', 'deleted')
-			ORDER BY links.source, links.type, links.target
